80d6f95aea07d1a940c037001778c31eb6cf1174
vmfesta <vmfesta@gmail.com>
2017-07-03 11:48:28 -0400

    exported the get function

---
+++ http-functions.js
@@ -0,0 +1,19 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+    var resp = "";
+
+    https.get(options, function (response) {
+        response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+        resp += data + "\n";
+        console.log("entrou");
+    });
+
+    response.on('end', function() {
+        callback(resp);
+    });
+
+    });
+};
